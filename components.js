3ca1865d193ba31972eccbe166af39269cf01970
Hossam Khalil <hossam.m.khalil@gmail.com>
2019-06-09 15:28:25 +0200

    Using init() from lifecycle handler

---
+++ components.js
@@ -0,0 +1,10 @@
+// register first component
+AFRAME.registerComponent('log', {
+    schema: {
+        message: {type: 'string', default: 'hello, world!'}
+    },
+    init: function () {
+        console.log(this.data.message);   
+    }
+});
+
