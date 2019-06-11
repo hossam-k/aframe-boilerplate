e51ccecb6fe2e714a8bae530fb6f1bdfb1057949
Hossam Khalil <hossam.m.khalil@gmail.com>
2019-06-11 18:30:17 +0200

    handling events with JS

--- components.js
+++ components.js
@@ -92,3 +92,21 @@ AFRAME.registerComponent('follow',{
     });
   }
 });
+
+AFRAME.registerComponent('change-color-on-hover', {
+  schema:{
+    color: {default: 'red'}
+  },
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+    var defaultColor = el.getAttribute('material').color;
+    el.addEventListener('mouseenter', function(){
+      el.setAttribute('color', data.color);
+    });
+
+    el.addEventListener('mouseleave', function(){
+      el.setAttribute('color', defaultColor);
+    });
+  }
+});
